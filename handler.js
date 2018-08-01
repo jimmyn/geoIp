7ddd073b79bdcdc367afd9e1af49fc6b6415d282
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-08-01 18:12:06 +0300

    Initial commit

---
+++ handler.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const request = require('request');
+
+const supportedOrigins = [
+  'https://microapps.fwd.wf',
+  'https://monei.net',
+  'https://moonmail.io'
+];
+
+module.exports.locate = (event, context, callback) => {
+  console.log(JSON.stringify(event, null, 2));
+  const origin = event.headers.origin;
+  if (!supportedOrigins.includes(origin)) {
+    const result = {
+      statusCode: 401,
+      body: JSON.stringify({
+        message: 'Not allowed'
+      }),
+    };
+
+    callback(null, result);
+  }
+  const options = {
+    method: 'GET',
+    url: `https://pro.ip-api.com/json/${event.requestContext.identity.sourceIp}`,
+    qs: {key: process.env.API_KEY}
+  };
+
+  request(options, function (error, response, body) {
+    if (error) callback(error);
+    const result = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Credentials': true,
+      },
+      body,
+    };
+
+    callback(null, result);
+  });
+};
